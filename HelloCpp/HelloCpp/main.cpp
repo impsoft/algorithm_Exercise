3f194e27f2af1c2892d1b83dc0f4a16e64b65a10
Wilson <impsoft@icloud.com>
2017-08-08 22:24:05 +0900

    Add Readme.txt

---
+++ HelloCpp/HelloCpp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HelloCpp
+//
+//  Created by 김상영 on 2017. 8. 2..
+//  Copyright © 2017년 impsoft. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
